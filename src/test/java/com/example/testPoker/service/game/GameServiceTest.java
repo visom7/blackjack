81f43bfd5184d6625372833ba239009871e92580
visom7 <Isabel977*>
2019-04-05 12:58:06 +0200

    190402 - Unit test for GameService

--- src/test/java/com/example/testPoker/service/game/GameServiceTest.java
+++ src/test/java/com/example/testPoker/service/game/GameServiceTest.java
@@ -2,6 +2,7 @@ package com.example.testPoker.service.game;
 
 import com.example.testPoker.model.Player;
 import com.example.testPoker.model.game.Card;
+import com.example.testPoker.model.game.PlayerStatus;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -10,11 +11,16 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyListOf;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @Test
@@ -67,5 +73,81 @@ public class GameServiceTest extends AbstractTestNGSpringContextTests {
 
         //Then
         Assert.assertEquals(size, 4);
+    }
+
+    @Test
+    public void testGetStartingHandGeneric() {
+        //Given
+        List<Card> hand = new ArrayList<>();
+        hand.add(Mockito.mock(Card.class));
+
+        when(cardPicker.pickCardFromDeck(anyListOf(Card.class))).thenReturn(Mockito.mock(Card.class));
+        when(player.getHand()).thenReturn(hand);
+        when(scoreCalculator.isBlackJack(hand)).thenReturn(false);
+
+        //When
+        gameService.getStartingHand();
+
+        //Then
+        verify(player, times(1)).cleanHand();
+        verify(player, times(1)).setPlayerStatus(PlayerStatus.UNDER_21);
+        verify(player, times(2)).addCardToHand(any(Card.class));
+        verify(player, times(0)).setPlayerStatus(PlayerStatus.BLACKJACK);
+    }
+
+    @Test
+    public void testGetStartingHandWithBlackjack() {
+        //Given
+        List<Card> hand = new ArrayList<>();
+        hand.add(Mockito.mock(Card.class));
+
+        when(cardPicker.pickCardFromDeck(anyListOf(Card.class))).thenReturn(Mockito.mock(Card.class));
+        when(player.getHand()).thenReturn(hand);
+        when(scoreCalculator.isBlackJack(hand)).thenReturn(true);
+
+        //When
+        gameService.getStartingHand();
+
+        //Then
+        verify(player, times(1)).cleanHand();
+        verify(player, times(1)).setPlayerStatus(PlayerStatus.UNDER_21);
+        verify(player, times(2)).addCardToHand(any(Card.class));
+        verify(player, times(1)).setPlayerStatus(PlayerStatus.BLACKJACK);
+    }
+
+    @DataProvider(name = "testGetCardDataProvider")
+    public Object[][] testGetCardDataProvider() {
+        return new Object[][]{
+                {21, PlayerStatus.JUST_21},
+                {22, PlayerStatus.OVER_21}
+        };
+    }
+
+    @Test(dataProvider = "testGetCardDataProvider")
+    public void testGetCardWithoutAddingCard(int score, PlayerStatus playerStatus) {
+        //Given
+        when(cardPicker.pickCardFromDeck(anyListOf(Card.class))).thenReturn(Mockito.mock(Card.class));
+        when(player.getScore()).thenReturn(score);
+
+        //When
+        gameService.getCard();
+
+        //Then
+        verify(player, times(0)).addCardToHand(any(Card.class));
+        verify(player, times(1)).setPlayerStatus(playerStatus);
+    }
+
+    @Test
+    public void testGetCardWhenAddingCard() {
+        //Given
+        when(cardPicker.pickCardFromDeck(anyListOf(Card.class))).thenReturn(Mockito.mock(Card.class));
+        when(player.getScore()).thenReturn(20);
+
+        //When
+        gameService.getCard();
+
+        //Then
+        verify(player, times(1)).addCardToHand(any(Card.class));
+        verify(player, times(1)).setPlayerStatus(PlayerStatus.UNDER_21);
     }
 }
